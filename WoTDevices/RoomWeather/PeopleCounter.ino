2c2348d1dae5884414f0e605a81a7a432cad0c5f
Laura <laura.bugo@studio.unibo.it>
2020-06-17 15:39:32 +0200

    Adapt to nodemcu

--- WoTDevices/RoomWeather/PeopleCounter.ino
+++ WoTDevices/RoomWeather/PeopleCounter.ino
@@ -1,68 +1,74 @@
-#define timeSeconds 5se00
 
+#define timeSeconds 500
 
-int ir1 = 2;
-int ir2 = 3;
+const int IR1 = 12;
+const int IR2 = 14;
 
 int T1;
 int T2;
 
 int S;
 
-
 // Timer: Auxiliary variables
 unsigned long now = millis();
 unsigned long lastTrigger = 0;
 boolean startTimer = false;
 
-
-
 void setup() {
   Serial.begin(115200);
-  attachInterrupt(digitalPinToInterrupt(ir1),ISR1,RISING);
-  attachInterrupt(digitalPinToInterrupt(ir2),ISR2,RISING);
+  Serial.println("Starting...");
+  pinMode(IR1, INPUT_PULLUP);
+  pinMode(IR2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(IR1),ISR1,RISING);
+  attachInterrupt(digitalPinToInterrupt(IR2),ISR2,RISING);
+  Serial.println("Starting...");
 }
 
-void ISR1()
-{
+ICACHE_RAM_ATTR void ISR1() {
   T1 = millis(); 
-  Serial.print("1 DETECTED!!!");
-  Serial.println(T1);
   if (T2 == 0) {
     startTimer = true;
   }
   lastTrigger = millis();
 }
 
-void ISR2()
-{
+
+ICACHE_RAM_ATTR void ISR2() {
   T2 = millis();
-  Serial.print("2 DETECTED!!!");
-  Serial.println(T2);
   if (T1 == 0) {
     startTimer = true;
   }  lastTrigger = millis();
 }
 
 
+boolean firstEventEnter = 0;
+boolean firstEventExit = 0;
+int peopleCounter = 0;
+
 void loop() 
 {
-  
- 
-  if(T1 > T2 && T1 != 0 && T2 != 0) 
-  {
-     Serial.println("ENTER");
+  if(T1 > T2 && T1 != 0 && T2 != 0 && firstEventEnter == 0) {
+    peopleCounter ++;
+    Serial.print("Entrance - ");
+    Serial.println(peopleCounter);
+    firstEventEnter = 1;
+    firstEventExit = 0;
   }
-  else if (T2 > T1 && T1 != 0 && T2 != 0) 
-  {
-    Serial.println("EXIT");
+  else if (T2 > T1 && T1 != 0 && T2 != 0 && firstEventExit == 0) {
+    peopleCounter --;
+    Serial.print("Exit - ");
+    Serial.println(peopleCounter);
+    firstEventEnter = 0;
+    firstEventExit = 1;
   }
 
+  //Serial.println(peopleCounter);
   now = millis();
   if(startTimer && (now - lastTrigger > (timeSeconds))) {
-    Serial.println("Motion stopped...");
     T1 = 0;
     T2 = 0;
+    firstEventEnter = 0;
+    firstEventExit = 0;
     startTimer = false;
   }
 
