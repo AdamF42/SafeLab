6bfcfff8863d0434dba35ed523a54464beb275c5
Adamo Fapohunda <adamofapohunda@gmail.com>
2020-07-21 23:44:23 +0200

    Fix variable name

--- AlertBot/src/config/constants.ts
+++ AlertBot/src/config/constants.ts
@@ -1,3 +1,3 @@
 export const PORT = process.env.PORT || 4000;
-export const TOKEN = process.env.TOKEN || "";
+export const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN || "";
 export const CHAT_ID = process.env.CHAT_ID || 123;
